9dac8d4eacb4b8ddfce52da1469c85e6ca7d99c6
msyml <18858713997@163.com>
2021-12-16 20:42:52 +0800

    feat:footer组件

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
 import Header from '@/components/Header/index.vue'
+import Footer from '@/components/Footer/index.vue'
 </script>
 
 <template>
   <div>
     <Header></Header>
     <router-view></router-view>
+    <Footer></Footer>
   </div>
 </template>
 
